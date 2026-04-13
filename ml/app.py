57806edcf5deee660369b1b6047bbba95fcbf2c0
Sneha Suman <riyasneha262004@gmail.com>
2026-04-13 10:19:32 +0530

    Final MLOps setup with monitoring and DVC

--- ml/app.py
+++ ml/app.py
@@ -1,3 +1,5 @@
+import csv
+from datetime import datetime
 from fastapi import FastAPI
 from pydantic import BaseModel
 import joblib
@@ -5,8 +7,29 @@ import os
 
 app = FastAPI()
 
-model_path = os.path.join(os.path.dirname(__file__), "model.pkl")
+model_dir = os.path.join(os.path.dirname(__file__), "models")
+
+models = sorted([f for f in os.listdir(model_dir) if f.startswith("model_v")])
+
+latest_model = models[-1]
+
+model_path = os.path.join(model_dir, latest_model)
+
 model = joblib.load(model_path)
+
+def log_prediction(temp, humidity, prediction):
+    log_file = os.path.join(os.path.dirname(__file__), "logs.csv")
+
+    file_exists = os.path.isfile(log_file)
+
+    with open(log_file, mode='a', newline='') as file:
+        writer = csv.writer(file)
+
+        # Write header if file is new
+        if not file_exists:
+            writer.writerow(["timestamp", "temp", "humidity", "prediction"])
+
+        writer.writerow([datetime.now(), temp, humidity, prediction])
 
 class InputData(BaseModel):
     temp: float
@@ -19,4 +42,9 @@ def home():
 @app.post("/predict")
 def predict(data: InputData):
     pred = model.predict([[data.temp, data.humidity]])
-    return {"prediction": int(pred[0])}
+    result = int(pred[0])
+
+    # ✅ LOGGING
+    log_prediction(data.temp, data.humidity, result)
+
+    return {"prediction": result}
